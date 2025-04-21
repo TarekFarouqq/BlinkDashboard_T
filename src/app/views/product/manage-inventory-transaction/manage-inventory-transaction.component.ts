39c5982f4c6312f2e8425ccd095e52cfcde4db4d
proGamalSherif <jemyxsherifx@gmail.com>
2025-04-21 05:51:33 +0200

    final update on product section

--- src/app/views/product/manage-inventory-transaction/manage-inventory-transaction.component.ts
+++ src/app/views/product/manage-inventory-transaction/manage-inventory-transaction.component.ts
@@ -4,23 +4,76 @@ import { FormsModule } from '@angular/forms';
 import { SpinnerComponent } from '@coreui/angular';
 import { TransactionHistoryService } from '../../../../services/transaction-history.service';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-manage-inventory-transaction',
-  imports: [SpinnerComponent,FormsModule,CommonModule],
+  imports: [SpinnerComponent, FormsModule, CommonModule],
   templateUrl: './manage-inventory-transaction.component.html',
-  styleUrl: './manage-inventory-transaction.component.scss'
+  styleUrl: './manage-inventory-transaction.component.scss',
 })
 export class ManageInventoryTransactionComponent implements OnInit {
-  isLoading:boolean=false;
-  currentPage:number=1;
-  TransactionArr!:any[];
-  constructor(private transactionServ:TransactionHistoryService,private router:Router){}
+  isLoading: boolean = false;
+  currentPage!:number;
+  TransactionArr!: any[];
+  TotalPages!: number;
+  constructor(
+    private transactionServ: TransactionHistoryService,
+    private router: Router
+  ) {}
   ngOnInit() {
-    this.transactionServ.GetTransactionHistory().subscribe((res)=>{
-      this.TransactionArr=res;
+   this.transactionServ.GetTotalPages(10).subscribe({
+    next:(res)=>{
+      this.TotalPages=res;
+      this.currentPage=this.TotalPages;
+      this.transactionServ.GetDataWithPagination(this.currentPage,10).subscribe({
+        next:(response)=>{
+          this.TransactionArr=response;
+        }
+      })
+    }
+   })
+  }
+  NavigateToDetails(id: number) {
+    this.router.navigate(['/product/transaction-details/', id]);
+  }
+  ShowErrorPanel(msg: string) {
+    Swal.fire({
+      toast: true,
+      position: 'top-end',
+      theme: 'light',
+      icon: 'error',
+      title: msg,
+      showConfirmButton: false,
+      timer: 2500,
+    });
+    this.isLoading = false;
+  }
+  ShowSuccessPanel(msg: string) {
+    Swal.fire({
+      toast: true,
+      position: 'top-end',
+      theme: 'light',
+      icon: 'success',
+      title: msg,
+      showConfirmButton: false,
+      timer: 2500,
+    });
+    this.isLoading = false;
+  }
+  nextPage(){
+    this.currentPage++;
+    this.transactionServ.GetDataWithPagination(this.currentPage,10).subscribe({
+      next:(response)=>{
+        this.TransactionArr=response;
+      }
     })
   }
-  NavigateToDetails(id:number){
-    this.router.navigate(['/product/transaction-details/', id]);
+  prevPage(){
+    this.currentPage--;
+    this.transactionServ.GetDataWithPagination(this.currentPage,10).subscribe({
+      next:(response)=>{
+        this.TransactionArr=response;
+      }
+    })
   }
 }
