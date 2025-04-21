85039ad988053c8e4ee0ddc741a07d4ffc75949d
tarekfarouqq <tarekahmedfarouq@gmail.com>
2025-04-22 01:39:25 +0200

    1234

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  //  apiUrl: 'http://blink-largeapi.runasp.net/api' // Large Database
-   apiUrl: 'http://blink-smallapi.runasp.net/api' // Small Database
+   apiUrl: 'http://blink-largeapi.runasp.net/api' // Large Database
+  //  apiUrl: 'http://blink-smallapi.runasp.net/api' // Small Database
   // apiUrl:'https://localhost:7027/api'    //local
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
  
-      //  apiUrl: 'http://blink-largeapi.runasp.net/api' // Large Database
-       apiUrl: 'http://blink-smallapi.runasp.net/api' // Small Database
+       apiUrl: 'http://blink-largeapi.runasp.net/api' // Large Database
+      //  apiUrl: 'http://blink-smallapi.runasp.net/api' // Small Database
       // apiUrl:'https://localhost:7027/api'
  
 };
