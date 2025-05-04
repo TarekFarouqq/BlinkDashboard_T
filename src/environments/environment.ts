26e1f6342c61a71c553804d9b1baeebd40b0e193
tarekfarouqq <tarekahmedfarouq@gmail.com>
2025-05-04 16:57:22 +0300

    SignalR

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
- 
-      //  apiUrl: 'http://blink-largeapi.runasp.net/api' // Large Database
-      //  apiUrl: 'http://blink-smallapi.runasp.net/api' // Small Database
-      apiUrl:'https://localhost:7027/api'
- 
+   //  apiUrl: 'http://blink-largeapi.runasp.net/api' // Large Database
+  //  apiUrl: 'http://blink-smallapi.runasp.net/api' // Small Database
+  apiUrl:'https://localhost:7027/api'    //local
+  ,
+  signalRUrl:'https://localhost:7027' //local
 };
